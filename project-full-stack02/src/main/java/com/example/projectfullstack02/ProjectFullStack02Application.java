e390f655b74260a0d495c05b6ab00762375cd8c4
VuDucLong1807 <44286058+VuDucLong1807@users.noreply.github.com>
2020-08-03 17:47:49 +0700

    Add files via upload

---
+++ project-full-stack02/src/main/java/com/example/projectfullstack02/ProjectFullStack02Application.java
@@ -0,0 +1,13 @@
+package com.example.projectfullstack02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFullStack02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFullStack02Application.class, args);
+    }
+
+}
---
+++ project-full-stack02/src/test/java/com/example/projectfullstack02/ProjectFullStack02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectfullstack02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFullStack02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
